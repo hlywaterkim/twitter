853bd1e3cad70ead1e730e49f7c2d758a2d11413
Sungsu Kim <kim.sungsu6732@gmail.com>
2021-10-22 23:42:38 +0900

    ✨ add React-Routes

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+export default firebase.initializeApp(firebaseConfig);
